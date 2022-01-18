7d5ca0c910aed0c4acc0de66804ce26b553eb95d
seacaboqn <seacaboqn@gmail.com>
2022-01-18 18:41:15 +0700

    refactor code

--- src/views/classes/class.js
+++ src/views/classes/class.js
@@ -1,32 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
-import {
-  BrowserRouter as Router,
-  generatePath,
-  Switch,
-  Route,
-  useHistory,
-  useParams,
-} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-import {
-  CAvatar,
-  CButton,
-  CButtonGroup,
-  CCard,
-  CCardBody,
-  CCardFooter,
-  CCardHeader,
-  CCol,
-  CProgress,
-  CRow,
-  CTable,
-  CTableBody,
-  CTableDataCell,
-  CTableHead,
-  CTableHeaderCell,
-  CTableRow,
-} from '@coreui/react'
 import { GetClass } from '../../api/class'
 
 const Class = () => {
@@ -46,12 +21,24 @@ const Class = () => {
 
   return (
     <div>
-      <p>Tên lớp học: {cls.name}</p>
-      <p>Mã lớp học: {cls.code}</p>
-      <p>Id: {cls.id}</p>
-      <p>Người tạo: {cls?.owner?.name}</p>
-      <p>Sô học viên: {cls?.students?.length}</p>
-      <p>Sô giảng viên: {cls?.teachers?.length + 1}</p>
+      <p>
+        <b>Tên lớp học:</b> {cls.name}
+      </p>
+      <p>
+        <b>Mã lớp học:</b> {cls.code}
+      </p>
+      <p>
+        <b>Id:</b> {cls.id}
+      </p>
+      <p>
+        <b>Người tạo:</b> {cls?.owner?.name}
+      </p>
+      <p>
+        <b>Số học viên:</b> {cls?.students?.length}
+      </p>
+      <p>
+        <b>Số giảng viên:</b> {cls?.teachers?.length + 1}
+      </p>
     </div>
   )
 }
