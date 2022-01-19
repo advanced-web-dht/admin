1120daac0a58ff916faa2cd556d5e71bb89bcf89
seacaboqn <seacaboqn@gmail.com>
2022-01-19 08:59:12 +0700

    fix

--- src/views/classes/classes.js
+++ src/views/classes/classes.js
@@ -14,6 +14,9 @@ import {
   CTableRow,
 } from '@coreui/react'
 import { GetAllClasses } from '../../api/class'
+import CIcon from '@coreui/icons-react'
+import { cilArrowBottom } from '@coreui/icons/js/free/cil-arrow-bottom'
+import { cilArrowTop } from '@coreui/icons/js/free/cil-arrow-top'
 
 const Classes = () => {
   const [sort, setSort] = useState('DESC')
@@ -75,7 +78,17 @@ const Classes = () => {
             <CTableHeaderCell scope="col">#</CTableHeaderCell>
             <CTableHeaderCell scope="col">Tên lớp học</CTableHeaderCell>
             <CTableHeaderCell scope="col">Code</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Ngày tạo</CTableHeaderCell>
+            <CTableHeaderCell scope="col">
+              <CButton
+                color="dark"
+                variant="ghost"
+                style={{ display: 'flex', alignItems: 'center' }}
+                onClick={() => setSort((prev) => (prev === 'DESC' ? 'ASC' : 'DESC'))}
+              >
+                Ngày tạo
+                <CIcon icon={sort === 'DESC' ? cilArrowBottom : cilArrowTop} size="lg" />
+              </CButton>
+            </CTableHeaderCell>
           </CTableRow>
         </CTableHead>
         <CTableBody>
